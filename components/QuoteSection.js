af70d9a1a773bbadd3ea56f9c5158f7845aa9e8b
sai2565 <saisharan2565@gmail.com>
2021-06-27 16:36:50 +0530

    fixed in login page for responsiveness

--- components/QuoteSection.js
+++ components/QuoteSection.js
@@ -4,8 +4,8 @@ function QuoteSection() {
             <div className="m-5 lg:mx-32 lg:grid lg:grid-cols-2">
                 <div className="lg:col-span-1">
                     <h1
-                        className="text-5xl text-[#29b2fe] font-baskerville lg:text-7xl"
-                        >Join the best freelance marketplace</h1>
+                        className="text-5xl text-[#29b2fe] font-baskerville lg:text-7xl">
+                            Join the best freelance marketplace</h1>
                     <div className="text-2xl font-semibold text-gray-500 mt-3">
                         <h1 >Find great talent. Build your business.</h1>
                         <h1>Take your career to the next level.</h1> 
